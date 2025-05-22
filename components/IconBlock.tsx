4da46c8336e7ef87db285621bff7c5369c0b9ed3
Florian Hitz <florian@raffiniert.biz>
2025-05-22 16:14:26 +0200

    icon block now has a link attribute

--- components/IconBlock.tsx
+++ components/IconBlock.tsx
@@ -2,22 +2,38 @@ import { storyblokEditable } from "@storyblok/react/rsc";
 import { IconBlockStoryblok } from "@/component-types-sb";
 import Title from "@/components/Title";
 import * as LucideIcons from "lucide-react";
+import Link from "next/link";
 
 const IconBlock: React.FunctionComponent<IconBlockStoryblok> = ({ blok }) => {
   const Icon = blok.icon && LucideIcons[blok.icon as keyof typeof LucideIcons];
-
-  return (
-    <div
-      {...storyblokEditable(blok)}
-      className="flex flex-row items-center gap-8 bg-card-muted rounded-lg p-6"
-    >
+  
+  const content = (
+    <>
       {Icon && (
-          <Icon className="h-6 w-6 text-primary" />
+        <Icon className="h-6 w-6 text-primary" />
       )}
       <div className="flex-1">
         <Title variant="h3" subvariant="secondary">{blok.title}</Title>
         <p>{blok.description || ""}</p>
       </div>
+    </>
+  );
+
+  return (
+    <div
+      {...storyblokEditable(blok)}
+      className={`flex flex-row items-center gap-8 bg-card-muted rounded-lg p-6`}
+    >
+      {blok.link ? (
+        <Link 
+          href={blok.link.url || '#'}
+          className="flex flex-row items-center gap-8 w-full h-full"
+        >
+          {content}
+        </Link>
+      ) : (
+        content
+      )}
     </div>
   );
 };
