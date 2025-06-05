a7235c148b87ced4cae069883c16308706eb81c9
Florian Hitz <florian@raffiniert.biz>
2025-06-05 21:56:46 +0200

    removed secure email sending

--- app/api/sendEmail/route.tsx
+++ app/api/sendEmail/route.tsx
@@ -16,8 +16,8 @@ export async function POST(req: Request) {
 
     const transporter = nodemailer.createTransport({
       host: process.env.SMTP_HOST,
-      port: Number(process.env.SMTP_PORT) || 465,
-      secure: true,
+      port: Number(process.env.SMTP_PORT) || 587,
+      secure: false,
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
